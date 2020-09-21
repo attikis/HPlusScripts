2ccba1c40807c642eaca391f1465cfd07c1f63bf
Alexandros Attikis <attikis@cern.ch>
2020-09-21 16:45:25 +0200

    update

--- python/plotTauFR.py
+++ python/plotTauFR.py
@@ -9,6 +9,7 @@ for that.
 USAGE:
 
 
+
 EXAMPLES: 
 
 
@@ -35,16 +36,17 @@ def getListOfFiles(dataset="Run2016B-DoubleMuon-NANOAOD-02Apr2020"):
     '''
     dasgoclient --query 'file dataset=/DoubleMuon/Run2016B-02Apr2020_ver2-v1/NANOAOD'
     '''
-    
+    basedir   = "/afs/cern.ch/user/a/attikis/workspace/nanoAOD/"
     fileLists = {}
-    fileLists["Run2016B-DoubleMuon-NANOAOD-02Apr2020"] = ["C4804A85-5EE9-ED4A-8F87-FE23D9A682AE_Skim.root",
-                                                          "AACB26B2-3924-3F48-9E27-30FAD0AD092F_Skim.root",
-                                                          "9F890C30-18E8-2D42-920F-5BDE836E3B57_Skim.root",
-                                                          "96200C93-DA5C-C145-A0E8-FB59D7E584A0_Skim.root",
-                                                          "7985E7AF-EC8F-C34D-9848-44A5E3EDE0A2_Skim.root",
-                                                          "792E70B0-2715-F24B-A8DE-1327E9F6480B_Skim.root",
-                                                          "3D4A9ACB-7ACC-AA4B-A3F1-F300B50D6196_Skim.root",
-                                                          "13CBB9A0-5CCA-0347-8C64-1F0A7E652071_Skim.root"]
+    fileLists["Run2016B-DoubleMuon-NANOAOD-02Apr2020"] = ["%sC4804A85-5EE9-ED4A-8F87-FE23D9A682AE_Skim.root" % (basedir),
+                                                          "%sAACB26B2-3924-3F48-9E27-30FAD0AD092F_Skim.root" % (basedir),
+                                                          "%s9F890C30-18E8-2D42-920F-5BDE836E3B57_Skim.root" % (basedir),
+                                                          "%s96200C93-DA5C-C145-A0E8-FB59D7E584A0_Skim.root" % (basedir),
+                                                          "%s7985E7AF-EC8F-C34D-9848-44A5E3EDE0A2_Skim.root" % (basedir),
+                                                          "%s792E70B0-2715-F24B-A8DE-1327E9F6480B_Skim.root" % (basedir),
+                                                          "%s3D4A9ACB-7ACC-AA4B-A3F1-F300B50D6196_Skim.root" % (basedir),
+                                                          "%s13CBB9A0-5CCA-0347-8C64-1F0A7E652071_Skim.root" % (basedir)]
+
     if dataset in fileLists.keys():
         return fileLists[dataset]
     else:
@@ -60,36 +62,55 @@ class ExampleAnalysis(Module):
 	Module.beginJob(self, histFile, histDirName)
         
         # Pt
-        self.hTauPt_LooseTau  = ROOT.TH1F('TauPt_LooseTau' , 'TauPt_LooseTau ; #tau_{h} p_{T} (GeV); Occur', 40,  0.0, 200.0)
+        self.hTauPt_LooseTau  = ROOT.TH1F('TauPt_LooseTau' , 'TauPt_LooseTau ; #tau_{h} p_{T} (GeV); Occur', 20,  0.0, 100.0)
         self.hTauEta_LooseTau = ROOT.TH1F('TauEta_LooseTau', 'TauEta_LooseTau; #tau_{h} #eta; Occur'       , 50, -2.5,   2.5)
+        self.hTauDM_LooseTau  = ROOT.TH1F('TauDM_LooseTau' , 'TauDM_LooseTau ; #tau_{h} decay mode; Occur' , 12,  0.0,  12.0)
+        self.hTauPtJetPtRatio_LooseTau = ROOT.TH1F('TauPtJetPtRatio_LooseTau', 'TauPtJetPtRatio_LooseTau ; #tau_{h} p_{T} / jet p_{T}; Occur', 100, 0.0, 5.0)
 
-        self.hTauPt_TightTau  = ROOT.TH1F('TauPt_TightTau' , 'TauPt_TightTau ; #tau_{h} p_{T} (GeV); Occur', 40,  0.0, 200.0)
-        self.hTauEta_TightTau = ROOT.TH1F('TauEta_TightTau', 'TauEta_TightTau; #tau_{h} #eta; Occur'       , 50, -2.5,   2.5)
-        self.hTauDM_TightTau  = ROOT.TH1F('TauDM_TightTau' , 'TauDM_TightTau ; #tau_{h} p_{T} (GeV); Occur', 12,  0.0,  12.0)
+        self.hTauPt_TightTau  = ROOT.TH1F('TauPt_TightTau'     , 'TauPt_TightTau      ; #tau_{h} p_{T} (GeV); Occur', 20,  0.0, 100.0)
+        self.hTauEta_TightTau = ROOT.TH1F('TauEta_TightTau'    , 'TauEta_TightTau     ; #tau_{h} #eta; Occur'       , 50, -2.5,   2.5)
+        self.hTauDM_TightTau  = ROOT.TH1F('TauDM_TightTau'     , 'TauDM_TightTau      ; #tau_{h} decay mode; Occur' , 12,  0.0,  12.0)
+        self.hTauPtJetPtRatio_TightTau = ROOT.TH1F('TauPtJetPtRatio_TightTau', 'TauPtJetPtRatio_TightTau ; #tau_{h} p_{T} / jet p_{T}; Occur', 100, 0.0, 5.0)
+
+        # Dimuon
+        self.hDimuonPt       = ROOT.TH1F('DimuonPt'  , 'DiumuonPt  ; #mu#mu p_{T}; Occur', 40,  0.0, 200.0)
+        self.hDimuonMass     = ROOT.TH1F('DimuonMass', 'DiumuonMass; #mu#mu invariant mass; Occur', 40,  0.0,  200.0)
+        self.hDimuonDeltaEta = ROOT.TH1F('DimuonDeltaEta', 'DiumuonDeltaEta; #mu#mu #Delta#eta; Occur', 50,  0.0,  5.0)
+        self.hDimuonDeltaPhi = ROOT.TH1F('DimuonDeltaPhi', 'DiumuonDeltaPhi; #mu#mu #Delta#phi (rads); Occur', 32, 0.0, 3.2)
+        self.hDimuonDeltaR   = ROOT.TH1F('DimuonDeltaR'  , 'DiumuonDeltaR  ; #mu#mu #DeltaR; Occur', 50,  0.0,  5.0)
 
         # Closest muon
-        self.hMuonDeltaPt_MinDeltaR    = ROOT.TH1F('MuonDeltaPt_MinDeltaR'   , 'MuonDeltaPt_MinDeltaR   ; p_{T}^{#tau_{h}} - p_{T}^{#mu} (GeV); Occur', 100, -5.0, 5.0)
-        self.hMuonDeltaR_MinDeltaR     = ROOT.TH1F('MuonDeltaR_MinDeltaR'    , 'MuonDeltaR_MinDeltaR    ; #DeltaR(#tau_{h}, #mu); Occur', 100, 0.0, 5.0)
+        self.hMuonDeltaPt_MinDeltaR    = ROOT.TH1F('MuonDeltaPt_MinDeltaR'   , 'MuonDeltaPt_MinDeltaR   ; (p_{T}^{#tau_{h}} - p_{T}^{#mu})/ p_{T}^{#tau_{h}}; Occur', 50, -5.0, 5.0)
+        self.hMuonDeltaR_MinDeltaR     = ROOT.TH1F('MuonDeltaR_MinDeltaR'    , 'MuonDeltaR_MinDeltaR    ; #DeltaR(#tau_{h}, #mu); Occur', 50, 0.0, 5.0)
         self.hMuonPt_MinDeltaR         = ROOT.TH1F('MuonPt_MinDeltaR'        , 'MuonPt_MinDeltaR        ; #mu p_{T} (GeV); Occur',  40,  0.0, 200.0)
         self.hMuonEta_MinDeltaR        = ROOT.TH1F('MuonEta_MinDeltaR'       , 'MuonEta_MinDeltaR       ; #mu #eta; Occur'       ,  50, -2.5,   2.5)
-        self.hMuonDxy_MinDeltaR        = ROOT.TH1F('MuonDxy_MinDeltaR'       , 'MuonDxy_MinDeltaR       ; d_{xy} (cm);Occur' , 200,  0.0,   2.0);
-        self.hMuonDz_MinDeltaR         = ROOT.TH1F('MuonDz_MinDeltaR'        , 'MuonDz_MinDeltaR        ; d_{z} (cm);Occur'   , 400,  0.0,  20.0);
-        self.hMuonSIP3D_MinDeltaR      = ROOT.TH1F('MuonSIP3D_MinDeltaR'     , 'MuonSIP3D_MinDeltaR     ; SIP_{3D}=|IP/#sigma_{IP}^{3D}|;Occur', 400, 0.0, 20.0);
-        self.hMuonMvaID_MinDeltaR      = ROOT.TH1F('MuonMvaID_MinDeltaR'     , 'MuonMvaID_MinDeltaR     ; MVA ID|;Occur', 5, 0.0, 5.0);
+        self.hMuonDxy_MinDeltaR        = ROOT.TH1F('MuonDxy_MinDeltaR'       , 'MuonDxy_MinDeltaR       ; d_{xy} (cm);Occur'  , 200,  0.0,  0.1);
+        self.hMuonDz_MinDeltaR         = ROOT.TH1F('MuonDz_MinDeltaR'        , 'MuonDz_MinDeltaR        ; d_{z} (cm);Occur'   , 200,  0.0,  0.);
+        self.hMuonSIP3D_MinDeltaR      = ROOT.TH1F('MuonSIP3D_MinDeltaR'     , 'MuonSIP3D_MinDeltaR     ; SIP_{3D}=|IP/#sigma_{IP}^{3D}|;Occur', 50, 0.0, 5.0);
+        self.hMuonMvaID_MinDeltaR      = ROOT.TH1F('MuonMvaID_MinDeltaR'     , 'MuonMvaID_MinDeltaR     ; MVA ID;Occur'    , 5, 0.0, 5.0);
         self.hMuonIsPFCand_MinDeltaR   = ROOT.TH1F('MuonIsPFCand_MinDeltaR'  , 'MuonIsPFCand_MinDeltaR  ; is PF cand;Occur', 2, 0.0, 2.0);
         self.hMuonIsTracker_MinDeltaR  = ROOT.TH1F('MuonIsTracker_MinDeltaR' , 'MuonIsTracker_MinDeltaR ; is tracker;Occur', 2, 0.0, 2.0);
         self.hMuonIsGlobal_MinDeltaR   = ROOT.TH1F('MuonIsGlobal_MinDeltaR'  , 'MuonIsGlobal_MinDeltaR  ; is Global;Occur' , 2, 0.0, 2.0);
         self.hMuonJetPtRelv2_MinDeltaR = ROOT.TH1F('MuonJetPtRelv2_MinDeltaR', 'MuonJetPtRelv2_MinDeltaR; jet p_{T}^{rel};Occur', 20, 0.0, 100.0);
-        self.hMuonJetRelIso_MinDeltaR  = ROOT.TH1F('MuonJetRelIso_MinDeltaR' , 'MuonJetRelIso_MinDeltaR ; jet rel iso;Occur', 200, 0.0, 20.0);
-        self.hMuonPFRelIso03_MinDeltaR = ROOT.TH1F('MuonPFRelIso03_MinDeltaR', 'MuonPFRelIso03_MinDeltaR; PF rel iso ;Occur', 200, 0.0, 20.0);
+        self.hMuonJetRelIso_MinDeltaR  = ROOT.TH1F('MuonJetRelIso_MinDeltaR' , 'MuonJetRelIso_MinDeltaR ; jet rel iso;Occur', 100, -0.5, 0.5);
+        self.hMuonPFRelIso03_MinDeltaR = ROOT.TH1F('MuonPFRelIso03_MinDeltaR', 'MuonPFRelIso03_MinDeltaR; PF rel iso ;Occur', 100, 0.0, 0.5);
 
         hList = []
         hList.append(self.hTauPt_LooseTau)
         hList.append(self.hTauEta_LooseTau)
+        hList.append(self.hTauDM_LooseTau)
+        hList.append(self.hTauPtJetPtRatio_LooseTau)
         #
         hList.append(self.hTauPt_TightTau)
         hList.append(self.hTauEta_TightTau)
         hList.append(self.hTauDM_TightTau)
+        hList.append(self.hTauPtJetPtRatio_TightTau)
+        #
+        hList.append(self.hDimuonPt)
+        hList.append(self.hDimuonMass)
+        hList.append(self.hDimuonDeltaEta)
+        hList.append(self.hDimuonDeltaPhi)
+        hList.append(self.hDimuonDeltaR)
         #
         hList.append(self.hMuonDeltaPt_MinDeltaR)
         hList.append(self.hMuonDeltaR_MinDeltaR)
@@ -133,17 +154,47 @@ class ExampleAnalysis(Module):
             raise Exception(msg)
 
     def analyze(self, event):
-        electrons = Collection(event, "Electron")
-        muons     = Collection(event, "Muon")
-        jets      = Collection(event, "Jet")
-        taus      = Collection(event, "Tau")
-        tau_p4    = ROOT.TLorentzVector()
-        looseTaus = []
-        tightTaus = []
+        electrons   = Collection(event, "Electron")
+        muons       = Collection(event, "Muon")
+        jets        = Collection(event, "Jet")
+        taus        = Collection(event, "Tau")
+        tau_p4      = ROOT.TLorentzVector()
+        looseTaus   = []
+        tightTaus   = []
+        jetsToClean = []
+        nTightMuons = 0
+        muon_p4     = [] #ROOT.TLorentzVector()
         
+        for mu in muons:
+            if (mu.mvaId>=1): #Mva ID from miniAOD selector (1=MvaLoose, 2=MvaMedium, 3=MvaTight)
+                jetsToClean.append(mu.jetIdx)
+                if (mu.mvaId==3):
+                    nTightMuons+= 1
+                    #muon_p4 += mu.p4()
+                    muon_p4.append(mu.p4())
+
+        if nTightMuons != 2:
+            return
+        if (muon_p4[0] + muon_p4[1]).M() >  91.1876 + 20:
+            return
+        if (muon_p4[0] + muon_p4[1]).M() < 91.1876 - 20:
+            return
+
+        # https://root.cern.ch/doc/master/classTLorentzVector.html
+        self.hDimuonPt.Fill( (muon_p4[0] + muon_p4[1]).Pt() )
+        self.hDimuonMass.Fill( (muon_p4[0] + muon_p4[1]).M() )
+        self.hDimuonDeltaEta.Fill(abs(muon_p4[0].Eta() - muon_p4[1].Eta()))
+        self.hDimuonDeltaPhi.Fill(muon_p4[0].DeltaPhi(muon_p4[1]))
+        self.hDimuonDeltaR.Fill(muon_p4[0].DeltaR(muon_p4[1]))
+
         # For-loop: All taus        
         for tau in taus:
 
+            # Cleaning (https://hypernews.cern.ch/HyperNews/CMS/get/physTools/3729/1.html)
+            if tau.jetIdx in jetsToClean:
+                continue
+
+            # Retrieve discriminators
             bPassVSe    = bool(tau.idDeepTau2017v2p1VSe >= 4)
             bPassVSmu   = bool(tau.idDeepTau2017v2p1VSmu >= 8)
             bPassVSjetL = bool(tau.idDeepTau2017v2p1VSjet >= self.getTauVSjetWP("vloose"))
@@ -158,6 +209,7 @@ class ExampleAnalysis(Module):
             # Save the deeptau (loose and tight)
             if (bPassVSjetL):
                 #print "VSjet = %.3f" % (tau.idDeepTau2017v2p1VSjet)
+                # print "tau.cleanmask = %d, tau.jetIdx = %d" % (tau.cleanmask, tau.jetIdx)
                 looseTaus.append(tau)
 
             if (bPassVSjetT):
@@ -171,7 +223,9 @@ class ExampleAnalysis(Module):
         # Fill histograms
         self.hTauPt_LooseTau.Fill(looseTaus[0].p4().Pt()) 
         self.hTauEta_LooseTau.Fill(looseTaus[0].p4().Eta()) 
-
+        self.hTauDM_LooseTau.Fill(looseTaus[0].decayMode)
+        if looseTaus[0].jetIdx > -1:
+            self.hTauPtJetPtRatio_LooseTau.Fill(looseTaus[0].p4().Pt() / jets[looseTaus[0].jetIdx].p4().Pt())
 
 	# Events with at least 1 tight tau
         if len(tightTaus) < 1: 
@@ -190,17 +244,20 @@ class ExampleAnalysis(Module):
         muonIsPFCand_min   = -1.0
         muonIsTracker_min  = -1.0
         muonIsGlobal_min   = -1.0
-        muonJetIdIndex_min = -1.0
+        muonJetPtRelv2_min = -1.0
         muonJetRelIso_min  = -1.0
         muonPFRelIso03_min = -1.0
 
         # For-loop: All muons
         for mu in muons:
+            if not (mu.mvaId>=1): #Mva ID from miniAOD selector (1=MvaLoose, 2=MvaMedium, 3=MvaTight)
+                continue
+
             # FIXME - add some quatlity cuts!  (e.g. loose muon)
             dR = tau_p4.DeltaR(mu.p4())
             if dR < dR_min:
                 dR_min  = dR
-                dPt_min = tau_p4.Pt() - mu.p4().Pt()
+                dPt_min = (tau_p4.Pt() - mu.p4().Pt())/tau_p4.Pt()
                 muonPt_min  = mu.p4().Pt()
                 muonEta_min = mu.p4().Eta()
                 muonDxy_min = mu.dxy
@@ -218,6 +275,8 @@ class ExampleAnalysis(Module):
         self.hTauPt_TightTau.Fill(tightTaus[0].p4().Pt()) 
         self.hTauEta_TightTau.Fill(tightTaus[0].p4().Eta()) 
         self.hTauDM_TightTau.Fill(tightTaus[0].decayMode)
+        if tightTaus[0].jetIdx > -1:
+            self.hTauPtJetPtRatio_TightTau.Fill(tightTaus[0].p4().Pt() / jets[tightTaus[0].jetIdx].p4().Pt())
         #
         self.hMuonPt_MinDeltaR.Fill(muonPt_min)
         self.hMuonEta_MinDeltaR.Fill(muonEta_min)
@@ -247,7 +306,7 @@ class ExampleAnalysis(Module):
 #preselection = "Tau_pt > 20 && fabs(Tau_eta) < 1.5 && Tau_decayMode == 0"
 preselection = "Tau_pt > 20 && Tau_decayMode == 0"
 preselection+= "&& @Muon_pt.size() >= 2"
-#preselection+= "&& @Tau_pt.size() >= 1"
+preselection+= "&& @Tau_pt.size() >= 1"
 
 #files=[" root://cms-xrd-global.cern.ch//store/mc/RunIISummer16NanoAOD/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2-v1/40000/2CE738F9-C212-E811-BD0E-EC0D9A8222CE.root"]
 
